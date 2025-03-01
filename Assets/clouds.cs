045443f8f157d156edb4adc62a3ae42131456c7f
anas23049 <kingdomhouse009@gmail.com>
2025-03-01 17:56:25 +0500

    Updated files

---
+++ Assets/clouds.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class clouds : MonoBehaviour
+{
+    public float resetPosition;     // X position where background resets
+
+    private Vector3 startPosition;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position += Vector3.left * 0.13f * Time.deltaTime;
+
+        // If it moves past resetPosition, move it back
+        if (transform.position.x < resetPosition)
+        {
+            transform.position = startPosition;
+        }
+    }
+}
