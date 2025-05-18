0114dcc016d0434d5713b5c497fda6ee0caca2ae
Khadija <bsai23006@itu.edu.pk>
2025-05-18 19:05:54 +0500

    added drugEffects basics

--- Assets/DrugEffect.cs
+++ Assets/DrugEffect.cs
@@ -1,3 +1,4 @@
+using Characters;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,12 @@ using UnityEngine.UI;
 public class DrugEffect : MonoBehaviour
 {
     public Slider drugSlider;
+    private Stats playerStats;
+
+    private void Start()
+    {
+        playerStats = GetComponent<Stats>();
+    }
 
     private void OnTriggerEnter(Collider collider)
     {
@@ -13,6 +20,35 @@ public class DrugEffect : MonoBehaviour
         {
             drugSlider.value += 10;
             Destroy(collider.gameObject);
+            ApplyDrugEffects();
         }
     }
+    public void ApplyDrugEffects()
+    {
+        StartCoroutine(ApplyTemporaryDrugEffects());
+    }
+
+    private IEnumerator ApplyTemporaryDrugEffects()
+    {
+        float originalSpeed = playerStats.speed;
+        float originalAcceleration = playerStats.acceleration;
+        float originalJumpForce = playerStats.jumpForce;
+        float originalHealth = playerStats.health;
+        
+        playerStats.speed *= 0.8f;
+        playerStats.acceleration *= 0.8f;
+        playerStats.jumpForce *= 0.8f;
+        playerStats.health -= 10f;
+     
+        yield return new WaitForSeconds(2f);
+   
+        playerStats.speed = originalSpeed;
+        playerStats.acceleration = originalAcceleration;
+        playerStats.jumpForce = originalJumpForce;
+        playerStats.health = originalHealth;
+        //playerStats.additionalGravityForce = originalGravity;
+    }
+
 }
+
+
